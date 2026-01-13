c9f3f57f6e5a233515bc71839d1546f6288572e9
Alia Collins <aliacollinseve@gmail.com>
2026-01-13 16:48:30 +0530

    Alpha 5.2.0-alpha.9: Auto-launch EVEMon after silent update completes

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("5.2.0.8")]
-[assembly: AssemblyFileVersion("5.2.0.8")]
-[assembly: AssemblyInformationalVersion("5.2.0-alpha.8")]
+[assembly: AssemblyVersion("5.2.0.9")]
+[assembly: AssemblyFileVersion("5.2.0.9")]
+[assembly: AssemblyInformationalVersion("5.2.0-alpha.9")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
